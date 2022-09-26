8661e76137c043178dceca0c287ff7985c87a4dc
yeongjin <dudwls901@naver.com>
2022-09-27 00:00:52 +0900

    [REFACTOR] SensorHistory Data Layer로 변경, PrimaryKey autoGenerate 추가

--- app/src/main/java/com/preonboarding/sensordashboard/data/model/SensorHistory.kt
+++ app/src/main/java/com/preonboarding/sensordashboard/data/model/SensorHistory.kt
@@ -1,4 +1,18 @@
 package com.preonboarding.sensordashboard.data.model
 
-//바로 domain model 사용시 삭제 예정
-//data class SensorHistory()
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "sensor_history_table")
+data class SensorHistory(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long,
+    val type: String,
+    @ColumnInfo(defaultValue = "CURRENT_TIMESTAMP")
+    val publishedAt: String,
+    val period: Double,
+//    val xList: List<Double>,
+//    val yList: List<Double>,
+//    val zList: List<Double>,
+)
--- app/src/main/java/com/preonboarding/sensordashboard/domain/model/SensorHistory.kt
+++ app/src/main/java/com/preonboarding/sensordashboard/domain/model/SensorHistory.kt
@@ -1,15 +1,8 @@
 package com.preonboarding.sensordashboard.domain.model
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "sensor_history_table")
 data class SensorHistory(
-    @PrimaryKey
     val id: Long,
     val type: String,
-    @ColumnInfo(defaultValue = "CURRENT_TIMESTAMP")
     val publishedAt: String,
     val period: Double,
 //    val xList: List<Double>,
