db7273c03571b42e4c2f1152064e820ddd4669a4
yeongjin <dudwls901@naver.com>
2022-09-26 18:18:54 +0900

    [Add] Data Layer 셋팅 마무리

---
+++ app/src/main/java/com/preonboarding/sensordashboard/data/mapper/SensorHistoryMapper.kt
@@ -0,0 +1,5 @@
+package com.preonboarding.sensordashboard.data.mapper
+
+//바로 domain model 사용시 삭제 예정
+object SensorHistoryMapper {
+}
---
+++ app/src/main/java/com/preonboarding/sensordashboard/data/model/SensorHistory.kt
@@ -0,0 +1,4 @@
+package com.preonboarding.sensordashboard.data.model
+
+//바로 domain model 사용시 삭제 예정
+//data class SensorHistory()
