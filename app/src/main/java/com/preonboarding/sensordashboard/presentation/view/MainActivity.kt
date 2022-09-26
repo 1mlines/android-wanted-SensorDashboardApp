a6cc8dd3386d936c0a142ce1d8bb0cbac463eff8
yeongjin <dudwls901@naver.com>
2022-09-27 00:31:03 +0900

    [CHORE] MainActivity view 패키지로 이동

--- app/src/main/java/com/preonboarding/sensordashboard/presentation/view/MainActivity.kt
+++ app/src/main/java/com/preonboarding/sensordashboard/presentation/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.preonboarding.sensordashboard.presentation
+package com.preonboarding.sensordashboard.presentation.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
