6a8e16f8df83a87a6eedfba2888c6ace90e38c74
YeongJin Kim <66052467+dudwls901@users.noreply.github.com>
2022-09-26 21:58:50 +0900

    [REFACTOR] BaseFragment bind 방식 변경
    Co-authored-by: Kim Hyun Su <86879099+KimHance@users.noreply.github.com>

--- app/src/main/java/com/preonboarding/sensordashboard/common/base/BaseFragment.kt
+++ app/src/main/java/com/preonboarding/sensordashboard/common/base/BaseFragment.kt
@@ -9,7 +9,26 @@ import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 
-abstract class BaseFragment<T : ViewDataBinding>(@LayoutRes val layoutRes: Int) : Fragment() {
+abstract class BaseFragment<VB : ViewDataBinding>(
+    @LayoutRes private val layoutId: Int
+) : Fragment(layoutId) {
+    private var _binding: VB? = null
+    protected val binding: VB
+        get() = requireNotNull(_binding)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = DataBindingUtil.bind(view)
+        with(binding) {
+            lifecycleOwner = viewLifecycleOwner
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
 
     private var _binding: T? = null
     protected val binding: T
