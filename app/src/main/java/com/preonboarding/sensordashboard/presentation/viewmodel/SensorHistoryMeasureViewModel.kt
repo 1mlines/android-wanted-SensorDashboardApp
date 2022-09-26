7e4f0a09dc46e7e4773da5d5dc42a4d370a75bfa
yeongjin <dudwls901@naver.com>
2022-09-27 00:30:38 +0900

    [CHORE] MeasureViewModel viewmodel 패키지로 이동

--- app/src/main/java/com/preonboarding/sensordashboard/presentation/viewmodel/SensorHistoryMeasureViewModel.kt
+++ app/src/main/java/com/preonboarding/sensordashboard/presentation/viewmodel/SensorHistoryMeasureViewModel.kt
@@ -1,18 +1,16 @@
-package com.preonboarding.sensordashboard.presentation.sensor_history_measure
+package com.preonboarding.sensordashboard.presentation.viewmodel
 
-import androidx.lifecycle.ViewModel
 import com.preonboarding.sensordashboard.common.base.BaseViewModel
-import com.preonboarding.sensordashboard.domain.model.SensorHistory
 import com.preonboarding.sensordashboard.domain.usecase.SaveSensorHistoryUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class SensorHistoryMeasureViewModel @Inject constructor(
-    private val saveSensorHistoryUseCase: SaveSensorHistoryUseCase
+    private val saveSensorHistoryUseCase: SaveSensorHistoryUseCase,
 ) : BaseViewModel() {
 
-    fun saveSensorHistory(){
+    fun saveSensorHistory() {
 //        saveSensorHistoryUseCase()
     }
 }
