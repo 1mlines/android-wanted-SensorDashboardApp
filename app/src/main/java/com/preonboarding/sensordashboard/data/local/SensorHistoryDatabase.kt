0daa1b0bbed8f8c1ccccdf07b1c33aa63689f410
YeongJin Kim <66052467+dudwls901@users.noreply.github.com>
2022-09-26 22:06:30 +0900

    [CHORE] @Databse 어노테이션 인자 줄 바꿈
    Co-authored-by: Kim Hyun Su <86879099+KimHance@users.noreply.github.com>

--- app/src/main/java/com/preonboarding/sensordashboard/data/local/SensorHistoryDatabase.kt
+++ app/src/main/java/com/preonboarding/sensordashboard/data/local/SensorHistoryDatabase.kt
@@ -5,7 +5,11 @@ import androidx.room.RoomDatabase
 import com.preonboarding.sensordashboard.data.local.dao.SensorHistoryDao
 import com.preonboarding.sensordashboard.domain.model.SensorHistory
 
-@Database(entities = [SensorHistory::class], version = 1, exportSchema = false)
+@Database(
+   entities = [SensorHistory::class], 
+   version = 1, 
+   exportSchema = false
+)
 abstract class SensorHistoryDatabase : RoomDatabase() {
 
     abstract fun sensorHistoryDao(): SensorHistoryDao
