c504ca1c154252d91596f345349ef64b4d4589ef
yeongjin <dudwls901@naver.com>
2022-09-27 13:08:15 +0900

    [REFACTOR] BaseFragment bind 방식 변경

--- app/src/main/java/com/preonboarding/sensordashboard/common/base/BaseFragment.kt
+++ app/src/main/java/com/preonboarding/sensordashboard/common/base/BaseFragment.kt
@@ -1,28 +1,24 @@
 package com.preonboarding.sensordashboard.common.base
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 
-abstract class BaseFragment<T : ViewDataBinding>(@LayoutRes val layoutRes: Int) : Fragment() {
+abstract class BaseFragment<VB : ViewDataBinding>(
+    @LayoutRes val layoutRes: Int,
+) : Fragment(layoutRes) {
 
-    private var _binding: T? = null
-    protected val binding: T
+    private var _binding: VB? = null
+    protected val binding: VB
         get() = requireNotNull(_binding)
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        _binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = DataBindingUtil.bind(view)
         binding.lifecycleOwner = viewLifecycleOwner
-        return binding.root
     }
 
     override fun onDestroyView() {
