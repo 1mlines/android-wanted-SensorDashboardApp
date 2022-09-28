2b07f7c342c82082e3d6c5147e2acae9fe322797
yforyuri <yforyurirho@gmail.com>
2022-09-28 15:06:37 +0900

    [FEAT] add paging source, paging query

---
+++ app/src/main/java/com/preonboarding/sensordashboard/presentation/view/sensor_history_list/HistoryPagingSource.kt
@@ -0,0 +1,35 @@
+package com.preonboarding.sensordashboard.presentation.view.sensor_history_list
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.preonboarding.sensordashboard.data.local.dao.SensorHistoryDao
+import com.preonboarding.sensordashboard.data.local.entity.SensorHistoryEntity
+
+class HistoryPagingSource(
+    private val dao: SensorHistoryDao
+    ): PagingSource<Int, SensorHistoryEntity>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, SensorHistoryEntity> {
+        val page = params.key ?: 1
+        return try {
+            val items = dao.getSensorDataList(
+                page,
+                params.loadSize
+            )
+            LoadResult.Page(
+                data = items,
+                prevKey = if (page == 1) null else page - 1,
+                nextKey = if (items.isEmpty()) null else page + (params.loadSize / 5)
+            )
+        } catch (e: Exception) {
+            return LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, SensorHistoryEntity>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+}
